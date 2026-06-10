c078b1af6df03074783d926cdd4de8df9e5db453
ranithu-dulneh <135724819+ranithu-dulneh@users.noreply.github.com>
2026-06-10 08:38:38 +0530

    Revert "feat: Admin panel enhancements and optimizations"

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import { CartProvider } from "./context/CartContext";
 import ScrollToTop from "./components/ScrollToTop";
-import PopupAd from "./components/PopupAd";
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
 import Login from "./pages/Login";
